6872fa28d3b8734de2eece8af1eab36040d7ab06
abdza <abdullah.zainul@gmail.com>
2020-01-28 07:51:12 +0800

    Added user notification module

---
+++ src/main/java/portal/User/UserNotificationRepository.java
@@ -0,0 +1,11 @@
+package portal.User;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserNotificationRepository extends JpaRepository<UserNotification, Long> {
+	
+	List<UserNotification> findByUser(User user);
+
+}
