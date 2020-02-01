d440ff6f268659a3e7d52172fa2d6680307ee6fe
abdza <abdullah.zainul@gmail.com>
2020-02-02 07:08:47 +0800

    Added search of email and staffid for user api query

--- src/main/java/org/portalengine/portal/User/UserApiController.java
+++ src/main/java/org/portalengine/portal/User/UserApiController.java
@@ -1,11 +1,15 @@
 package org.portalengine.portal.User;
 
+
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -13,9 +17,24 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.Data;
+
 @RestController
 @RequestMapping("/api/users")
 public class UserApiController {
+	
+	@Data
+	private class ApiUser {
+		public ApiUser(String name2, String staffid2, String email2) {
+			// TODO Auto-generated constructor stub
+			this.name=name2;
+			this.staffid=staffid2;
+			this.email=email2;
+		}
+		String name;
+		String staffid;
+		String email;
+	}
 	
 	@Autowired
 	private UserService service;
@@ -26,12 +45,27 @@ public class UserApiController {
 		int page = 0;
 		int size = 20;
 		if(request.getParameter("page") != null && !request.getParameter("page").isEmpty()) {
-			page = Integer.parseInt(request.getParameter("page"));
+			page = Integer.parseInt(request.getParameter("page")) - 1;
 		}
 		if(request.getParameter("size") != null && !request.getParameter("size").isEmpty()) {
 			size = Integer.parseInt(request.getParameter("size"));
 		}
-		return service.getRepo().findAll(PageRequest.of(page, size));
+		String search = "";
+		List<User> toreturn = null;
+		if(request.getParameter("q")!=null) {
+			search = "%" + request.getParameter("q").replace(" ", "%") + "%";			
+			toreturn = service.getRepo().apiquery(search);
+		}
+		else {
+			toreturn = service.getRepo().findAll();
+		}
 		
+		ArrayList<ApiUser> userlist = new ArrayList<ApiUser>();
+		for(int i=0;i<toreturn.size();i++) {
+			User cu = toreturn.get(i);
+			userlist.add(new ApiUser(cu.getName(),cu.getStaffid(),cu.getEmail()));
+		}
+		map.put("content", userlist);
+		return map;
 	}
 }
--- src/main/java/org/portalengine/portal/User/UserRepository.java
+++ src/main/java/org/portalengine/portal/User/UserRepository.java
@@ -1,6 +1,12 @@
 package org.portalengine.portal.User;
 
+import java.util.List;
+
+import org.portalengine.portal.Tracker.Tracker;
+import org.portalengine.portal.Tracker.Transition.TrackerTransition;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 	
@@ -8,4 +14,7 @@ public interface UserRepository extends JpaRepository<User, Long> {
 	
 	User findByUsername(String username);
 
+	@Query("select tt from User tt where tt.name like :#{#search} or tt.email like :#{#search} or tt.staffid like :#{#search}")
+	List<User> apiquery(@Param("search") String search);
+	
 }
