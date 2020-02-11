eb14404ddb27c53a9e3dda89ea62ec053509ac70
abdza <abdullah.zainul@gmail.com>
2020-02-11 19:51:34 +0800

    Added servlet initializer for war package

---
+++ src/main/java/org/portalengine/portal/PortalServletInitializer.java
@@ -0,0 +1,12 @@
+package org.portalengine.portal;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class PortalServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(PortalApplication.class);
+	}
+}
